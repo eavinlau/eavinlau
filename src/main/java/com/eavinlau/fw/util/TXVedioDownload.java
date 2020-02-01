fb5a9604fe7bf9fa4e443f26fc157c618d941f24
liudebin <liudebin@ajplus319.anji-allways.local>
2020-02-01 13:34:25 +0800

    优化腾讯视频下载

--- src/main/java/com/eavinlau/fw/util/TXVedioDownload.java
+++ src/main/java/com/eavinlau/fw/util/TXVedioDownload.java
@@ -18,32 +18,27 @@ public class TXVedioDownload {
 
 
 	public static void main(String[] args) {
-		String vedioFileName="D:/eavinlau/忽而今夏/忽而今夏04.mp4";
-		String m3y8URL="https%3A%2F%2Fapd-1b409e8818cc1bd30a32cee835da00eb.v.smtcdns.com%2Fvipts.tc.qq.com%2FAv83t-KKJi0Ux85akb4KiDp0beWVdaj2_MsNDgzXv1DE%2FuwMROfz2r5xgoaQXGdGnC2df64_gcR4DH6oNabw6qBdCnk2E%2FOgGWcvobFIHs1MUS4qfQoLG2BHviVd4AUQv5G4Cz8kg9tfSY_t0gK3toRFxowaPSo33vNugZVLeKEKOkjb9F58plwgZFl7NIM3QAi3jrpr5n7IwCHvErV-rWP_veVN23bSFXNCMCD0qNO3A2U9Tq3ZYP7WoG84xDT47HYE7vw-I%2Fr00264m3wrh.321003.ts.m3u8";
-		String baseurl="https%3A%2F%2Fapd-1b409e8818cc1bd30a32cee835da00eb.v.smtcdns.com%2Fvipts.tc.qq.com%2FAv83t-KKJi0Ux85akb4KiDp0beWVdaj2_MsNDgzXv1DE%2FuwMROfz2r5xgoaQXGdGnC2df64_gcR4DH6oNabw6qBdCnk2E%2FOgGWcvobFIHs1MUS4qfQoLG2BHviVd4AUQv5G4Cz8kg9tfSY_t0gK3toRFxowaPSo33vNugZVLeKEKOkjb9F58plwgZFl7NIM3QAi3jrpr5n7IwCHvErV-rWP_veVN23bSFXNCMCD0qNO3A2U9Tq3ZYP7WoG84xDT47HYE7vw-I%2F";
+		String vedioFileName="D:/eavinlau/杀不死/杀不死01.mp4";
+		String m3y8URL="https%3A%2F%2Fapd-8ef03bc1431239b46c462d8fc034dcc8.v.smtcdns.com%2Fmoviets.tc.qq.com%2FAbuJ1vM8iaHWLsmVjJawD8ZfNo0qYkbd5MsM3q4mEtEA%2FuwMROfz2r5xgoaQXGdGnC2df64_gcR4DH6oNabw6qBdCnk2E%2Fpni8ZcNIlpr1ECs9SRFDgZArfTE7wqiSPtMBsW4DjY2QkZHdr6FF4qz9eTzmbc3KCQ8W1mWzmabQhA7skJk7_pUCG7_wVxqy-MPe95uoNavvr5gmx2P7OqKFOTIC27QtMb2klTZK_Tt58cfKv8hfcPfG-tTnXrzZXwSpagBcVcw%2Fo0024qwqegg.321003.ts.m3u8";
+		String baseurl=m3y8URL.substring(0,m3y8URL.lastIndexOf("%2F"))+"%2F";
 		URL url=null;
 		try {
 			url = new URL(m3y8URL.replaceAll("%2F", "/").replaceAll("%3A", ":"));
 			System.out.println(url);
-//			if(1==1) {
-//				return;
-//			}
-			InputStream is = getInputStream (url);
+			InputStream is = url.openStream();
 			// 读取响应
 			if(is!=null) {
 				BufferedReader reader = new BufferedReader(new InputStreamReader(is));
 				String line=null;
-//				byte[] buf = new byte[1024]; 
 				List<InputStream> list = new ArrayList<InputStream>();
 				Enumeration<InputStream> en =null;
 				String newurl="";
 				while ((line = reader.readLine()) != null) {
 					line = new String(line.getBytes(), "utf-8");
 					if(!line.startsWith("#EXT")) {
-//						FileUtils.copyURLToFile(new URL(line), new File(vedioFileName+i+vedioSuffix));
 						newurl=baseurl+line;
 						newurl=newurl.replaceAll("%2F", "/").replaceAll("%3A", ":");
-//						System.out.println(newurl);
+						System.out.println(newurl);
 						list.add(new URL(newurl).openStream());
 					}
 				}
